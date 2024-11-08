71ab4c22c19e125dee978316e5fb23afab70a3a2
Tyler Lemburg <trlemburg@gmail.com>
2024-11-08 17:16:59 -0600

    WIP

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,28 +1,29 @@
 # frozen_string_literal: true
 
 class Calc 
-  def add(x,y)
-    x+y
+  def arrays_same_length?(array1, array2)
+    array1.length == array2.length
   end
 
-	def get(hash, key)
-		hash[key]
-	end
+  def avg(array)
+    sum = 0
+    array.each do |val|
+      puts sum
+      sum += val
+    end
+    sum / array.length.to_f
+  end
 
-  def correct_addition?(x,y,z)
-		(x + y).equal?(z)
-	end
-
-	def strings_equal?(x, y)
-		x.eql?(y)
-	end
+  def median(array)
+    array.sort[array.length / 2]
+  end
 
 	def valid_upc_check_digit_gtin8?(array)
 		odds = [0,2,4,6].map do |i|
 			array.fetch(i)
 		end.inject(&:+)
 
-		evens = array[1] + array.fetch(3) + array.fetch(5)
+		evens = array[1] + array[3] + array[5]
 
 		((odds * 3 + evens) % 10).equal? array.fetch(7)
 	end
--- spec/calc_spec_fixed.rb
+++ spec/calc_spec_fixed.rb
@@ -3,24 +3,36 @@
 require_relative '../lib/calc'
 
 describe Calc do
+  # Change == to equal?
   describe '#arrays_same_length?' do
     it 'returns true when the arrays are the same length' do
       expect(Calc.new.arrays_same_length?(['a', 'b', 'c'], [1,2,3])).to be true
     end
+
+    it 'returns false when the arrays are not the same length' do
+      expect(Calc.new.arrays_same_length?(['a', 'b', 'c'], [0,1,2,3])).to be false
+    end
   end
 
+  # Note that mutant does not catch the case of 0-length array
+  # Remove the puts
   describe '#avg' do
     it 'averages an array of numbers' do
       expect(Calc.new.avg([1,2,3,4,5])).to eq 3
+      expect(Calc.new.avg([1,2,3,4,6])).to eq 3.2
     end
   end
 
   describe '#median' do
     it 'finds the median in an array' do
       expect(Calc.new.median([1,2,3,4,5])).to eq 3
+      expect(Calc.new.median([5,6,7,8,9])).to eq 7
+      expect(Calc.new.median([9,8,7,6,5])).to eq 7
+      expect(Calc.new.median([9,7,6,8,5])).to eq 7
     end
   end
 
+  # Use fetch for all array access
   describe '#valid_upc_check_digit_gtin8?' do
 		it 'returns true on a basic valid checksum' do
 			arr = [0,1,0,1,0,1,0,3]
@@ -31,5 +43,15 @@ describe Calc do
 			arr = [5,1,5,1,5,1,7,9]
 			expect(Calc.new.valid_upc_check_digit_gtin8?(arr)).to be true
 		end
+
+    it 'returns true on a basic valid checksum 3' do
+			arr = [5,2,5,3,5,4,7,5]
+			expect(Calc.new.valid_upc_check_digit_gtin8?(arr)).to be true
+		end
+
+    it 'returns false on an invalid checksum' do
+      arr = [5,1,5,1,5,0,7,9]
+			expect(Calc.new.valid_upc_check_digit_gtin8?(arr)).to be false
+    end
 	end
 end
